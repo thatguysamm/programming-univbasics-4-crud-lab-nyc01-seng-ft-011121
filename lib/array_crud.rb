a636513ee0dacffb172ff954f278de5a864685b5
friendly-format-0074 <samuelagasi@gmail.com>
2020-12-30 18:15:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,9 +9,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["wow", "I", "am", "really", "learning"]
-  element = "arrays!"
-  element<<
+array<< "arrays!"
+
 end
 
 def add_element_to_start_of_array(array, element)
